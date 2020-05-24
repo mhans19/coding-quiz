2c33bcca36b958976fa51289c9b841eb9b5a894e
mhans19 <mdhansen19@gmail.com>
2020-05-24 11:08:35 -0600

    extracted questions from for loop and organized quiz process

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,28 +7,50 @@ var buttonEl2 = document.querySelector("#opt2");
 var buttonEl3 = document.querySelector("#opt3");
 var buttonEl4 = document.querySelector("#opt4");
 var startbtnEl = document.querySelector("#startquiz");
+var timeLeft = 120;
 var score = 0;
-var choice = "";
+var questionIndex = 0;
 var questionOpts = [
     {q: "question 1",
-     a: [{opt: "answer 1"},
-         {opt: "answer 2"},
-         {opt: "answer 3"},
-         {opt: "answer 4"}],
-     correct: "buttonEl1"    
+     a: [{opt: "answer 1", correct: false},
+         {opt: "answer 2", correct: true},
+         {opt: "answer 3", correct: false},
+         {opt: "answer 4", correct: false}]   
     },
     {q: "question 2",
-     a: [{opt: "answer 1", correct: true},
-         {opt: "answer 2", correct: false},
-         {opt: "answer 3", correct: false},
-         {opt: "answer 4", correct: false}]
+     a: [{opt: "answer a", correct: true},
+         {opt: "answer b", correct: false},
+         {opt: "answer c", correct: false},
+         {opt: "answer d", correct: false}]
+    },
+    {q: "question 3",
+    a: [{opt: "answer 1", correct: false},
+        {opt: "answer 2", correct: true},
+        {opt: "answer 3", correct: false},
+        {opt: "answer 4", correct: false}]   
+    },
+    {q: "question 4",
+    a: [{opt: "answer 1", correct: false},
+        {opt: "answer 2", correct: true},
+        {opt: "answer 3", correct: false},
+        {opt: "answer 4", correct: false}]   
+    },
+    {q: "question 5",
+    a: [{opt: "answer 1", correct: false},
+        {opt: "answer 2", correct: true},
+        {opt: "answer 3", correct: false},
+        {opt: "answer 4", correct: false}]   
+    },
+    {q: "question 6",
+    a: [{opt: "answer 1", correct: false},
+       {opt: "answer 2", correct: true},
+       {opt: "answer 3", correct: false},
+       {opt: "answer 4", correct: false}]   
     }
     ];
 
 var countdown = function(){
-    var timeLeft = questionOpts.length * 20;
     var timeInterval = setInterval(function() {
-        //document.querySelector("startquiz")
         countdownEl.textContent = timeLeft;
         timeLeft--;
 
@@ -39,20 +61,32 @@ var countdown = function(){
     }, 1000)
 }
 
-
 var questionLoop = function(){
-    for (var i = 0; i < questionOpts.length; i++){
-        titleEl.textContent = questionOpts[i].q;
-
-        buttonEl1.textContent = questionOpts[i].a[0].opt;
-        buttonEl2.textContent = questionOpts[i].a[1].opt;
-        buttonEl3.textContent = questionOpts[i].a[2].opt;
-        buttonEl4.textContent = questionOpts[i].a[3].opt;
-
-        break;
-    }
+    //add what to do when we run out of questions (end function with results + high score)
+    selectedOpt = questionOpts[questionIndex];
+    titleEl.textContent = selectedOpt.q;
+    buttonEl1.textContent = selectedOpt.a[0].opt;
+    buttonEl2.textContent = selectedOpt.a[1].opt;
+    buttonEl3.textContent = selectedOpt.a[2].opt;
+    buttonEl4.textContent = selectedOpt.a[3].opt;
 }
 
+// var questionLoop = function(questionIndex){
+//     for (var i = 0; i < questionOpts.length; i++){
+//         titleEl.textContent = questionOpts[i].q;
+//         buttonEl1.textContent = questionOpts[i].a[0].opt;
+//         buttonEl2.textContent = questionOpts[i].a[1].opt;
+//         buttonEl3.textContent = questionOpts[i].a[2].opt;
+//         buttonEl4.textContent = questionOpts[i].a[3].opt;
+
+//         break;
+//     }
+// }
+
+var nextQuestion = function(){
+    questionIndex++;
+    questionLoop();
+}
 var runQuiz = function() {
     startbtnEl.remove();
     sectitleEl.remove();
@@ -61,36 +95,46 @@ var runQuiz = function() {
     questionLoop();
 }
 
-        // buttonEl1.addEventListener("click", function() {
-        //     if (loopObj.correct === "buttonEl1"){
-        //         choice = "buttonEl1";
-        //     }
-            
-        //   });
+///////////// need to add deduction from timeleft global variable
+buttonEl1.addEventListener("click", function() {
+    if (questionOpts[questionIndex].a[0].correct === true){
+        alert("You got this one right");
+        score ++;
+    } else {
+        alert("better luck next time");
+    }
+    nextQuestion();
+    });
 
-        // buttonEl2.addEventListener("click", function() {
-        //     if (loopObj.correct === "buttonEl2"){
-        //         choice = "buttonEl2";
-        //     }
-        // });  
+buttonEl2.addEventListener("click", function() {
+    if (questionOpts[questionIndex].a[1].correct === true){
+        alert("You got this one right");
+        score ++;
+    } else {
+        alert("better luck next time");
+    }
+    nextQuestion();
+    }); 
 
-        // buttonEl3.addEventListener("click", function() {
-        //     if (loopObj.correct === "buttonEl3"){
-        //         choice = "buttonEl3";
-        //     }
-        //   });
+buttonEl3.addEventListener("click", function() {
+    if (questionOpts[questionIndex].a[2].correct === true){
+        alert("You got this one right");
+        score ++;
+    } else {
+        alert("better luck next time");
+    }
+    nextQuestion();
+    }); 
 
-        // buttonEl4.addEventListener("click", function() {
-        //     if (loopObj.correct === "buttonEl4"){
-        //         choice = "buttonEl4";
-        //     }
-        // }); 
-
-        // if (choice === loopObj.correct){
-        //     alert("Congrats, you got it right!");
-        // } else {
-        //     alert("You did not get it correct.")
-        // }
+buttonEl4.addEventListener("click", function() {
+    if (questionOpts[questionIndex].a[3].correct === true){
+        alert("You got this one right");
+        score ++;
+    } else {
+        alert("better luck next time");
+    }
+    nextQuestion();
+    }); 
 
 
 
